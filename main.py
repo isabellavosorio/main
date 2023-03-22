2855d797d894d4d207c389e84bc94363b3b700d3
CSanTL <109187769+CSanTL@users.noreply.github.com>
2023-03-22 16:32:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ def encode(password):
     for digit in password:
         encoded_password += str((int(digit) + 3) % 10)
     print("The original password is", password, ", and the encoded password is", encoded_password)
+    
+    
+def decode(encoded_password):
+    decoded_password = ''
+    for digit in encoded_password:
+        decoded_password += str((int(digit) - 3) % 10)
+    print("The encoded password is", encoded_password, ", and the original password is", decoded_password)
 
 
 #Start of the loop
@@ -18,5 +25,6 @@ while True:
         encode(password)
 #This is the decoder option. It decodes the original encoded password.
     elif choice == 2:
+        decode(encoded_password)
     elif choice == 3:
         break
